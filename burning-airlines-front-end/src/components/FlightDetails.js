bbdb21c2182df48a358c70630b10fed41e4bf8a0
Laurence Qi <laurence.qi@outlook.com>
2022-02-15 22:18:24 +1100

    added main and flightdetails component classes

---
+++ burning-airlines-front-end/src/components/FlightDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class FlightDetails extends React.Component {
+    render() {
+        return (
+            <div>
+                
+                Flight details. 
+            </div>
+        )
+    }
+}
---
+++ burning-airlines-front-end/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FlightDetails from "./FlightDetails";
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                
+                Hello this is the Main component
+            </div>
+        )
+    }
+}
